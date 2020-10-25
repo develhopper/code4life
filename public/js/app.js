a3eb25b900e12614ccfc21c0266f94591aa147b3
develhopper <develhoper.0@gmail.com>
2020-10-25 22:41:58 +0330

    comment toast messages added

--- public/js/app.js
+++ public/js/app.js
@@ -2,23 +2,40 @@ $(function(){
     $("[data-toggle]").on("click",function(){
         $($(this).attr("data-toggle")).toggle('hide');
     });
+
     $("#comment-form").on("submit",function(e){
         e.preventDefault();
         var form=$(this);
         var submit=form.find(":input[type=submit]");
         submit.prop('disabled', true);
-        submit.val("در حال ارسال ...");
+        submit.addClass("bg-blue-grey");
         $.ajax({
             type:form.attr("method"),
             url:form.attr("action"),
             data:form.serialize(),
             success:function(data){
-                console.log(data);
-                submit.val("ارسال شد");
+                submit.removeClass("bg-blue-grey");
+                submit.prop('disabled', false);
+                make_toast("Your comment posted successfuly :)");
             },
             error:function(data){
-                console.log(data);
+                submit.removeClass("bg-blue-grey");
+                submit.prop('disabled', false);
+                make_toast("Somthing wrong happened, we lost your comment :(","danger");
             }
         });
     });
 });
+
+function make_toast(message,type="primary",rtl=false){
+    type="bg-"+type;
+    if(rtl==true)
+        type+=" rtl";
+    var toast=$("<div></div>").addClass("toast "+type).attr("id","toast-id");
+    var msg=$("<span></span>").addClass("message").text(message);
+    var close=$("<span></span>").addClass("close").append($("<i></i>").addClass("icon-cancel"));
+    close.on("click",function(){
+        toast.remove();
+    });
+    toast.append(msg).append(close).appendTo($("body"));
+}
